50293eeb77a6e7caa5f4ffb63903850c647f2279
ryan <laslomcd@gmail.com>
2024-07-01 15:13:46 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+	return (
+		<div className="bg-purple-500">
+			<h1 className="text-xl">Hello world</h1>
+			<button className="btn">Click</button>
+		</div>
+	);
+}
+
+export default App;
