e48348a4fa9076c925014859a33e92952b116c72
ryan <laslomcd@gmail.com>
2024-07-17 17:15:02 -0400

    added search functionality

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext();
 
@@ -6,23 +7,50 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
-	const [users, setUsers] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
+	const initialState = {
+		users: [],
+		isLoading: false,
+	};
+	// const [users, setUsers] = useState([]);
+	// const [isLoading, setIsLoading] = useState(true);
 
-	const fetchUsers = async () => {
-		const response = await fetch(`${GITHUB_URL}/users`, {
+	const [state, dispatch] = useReducer(githubReducer, initialState);
+
+	// Get search results
+	const searchUsers = async (text) => {
+		setLoading();
+		const params = new URLSearchParams({
+			q: text,
+		});
+		const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
 			headers: {
 				Authorization: `token ${GITHUB_TOKEN}`,
 			},
 		});
 
-		const data = await response.json();
+		const { items } = await response.json();
 
-		setUsers(data);
-		setIsLoading(false);
+		dispatch({
+			type: "GET_USERS",
+			payload: items,
+		});
+		// setUsers(data);
+		// setIsLoading(false);
 	};
 
-	return <GithubContext.Provider value={{ users, isLoading, fetchUsers }}>{children}</GithubContext.Provider>;
+	const clearUsers = () => {
+		dispatch({
+			type: "CLEAR_USERS",
+		});
+	};
+
+	const setLoading = () => dispatch({ type: setLoading });
+
+	return (
+		<GithubContext.Provider value={{ users: state.users, isLoading: state.isLoading, searchUsers, clearUsers }}>
+			{children}
+		</GithubContext.Provider>
+	);
 };
 
 export default GithubContext;
